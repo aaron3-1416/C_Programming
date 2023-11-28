e70929c4c3105a5a9346c8437b485804eb3e35fb
aaron3-1416 <92693623+aaron3-1416@users.noreply.github.com>
2023-11-28 15:00:06 -0600

    tester

---
+++ tester.c
@@ -0,0 +1,25 @@
+San Luis Tlaxialtemalco, Xochimilco CDMX, México
+
+Acuerdo Familiar
+
+Martes a 28 de Noviembre de 2023
+
+
+Soledad Marcela Valdez Herrera en pleno uso de mis facultades mentales y sin que se ejerza presión alguna, ES MI VOLUNTAD que a mi fallecimiento mi hijo José Aarón González Valdez pase ser el ÚNICO HEREDERO de la parte proporcional que me corresponde del predio ubicado en "Cerrada 16 de Septiembre número 5, Alcaldía Xochimilco Código Postal 16610, CDMX, México" y el cual se encuentra en trámite de Intestado.
+
+Estoy completamente segura que mis hijas Rosa Isela González Valdez y Araceli González Valdez estarán en la mejor disposición en firmar la documentación correspondiente para que se lleve a cabo mi última voluntad.
+
+Los gastos que se deriven en la tramitación del Intestado en curso, para adjudicación de mi parte proporcional, correrán por cuenta de José Aarón González Valdez.
+
+Rosa Isela González Valdez y Araceli González Valdez firmarán la documentación que les sea requerida.
+
+Sra. Soledad Marcela Valdez Herrera (firma de acuerdo)
+
+Hijos
+
+Rosa Isela González Valdez (firma de acuerdo)
+
+Araceli González Valdez (firma de acuerdo)
+
+José Aarón González Valdez (firma de acuerdo)
+
